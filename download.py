307af016bd90b633ddde69124c64ec06a07915f1
Svetlana Yatsyk <78039821+Svetlana-Yatsyk@users.noreply.github.com>
2025-07-09 16:08:38 +0200

    adapted for ru

--- download.py
+++ download.py
@@ -2,7 +2,7 @@ import wikipedia, requests, os, sys
 from urllib.parse import unquote
 
 os.makedirs("output", exist_ok=True)
-wikipedia.set_lang("is")
+wikipedia.set_lang("ru")
 
 n = int(sys.argv[-1])
 while n > 0:
@@ -11,10 +11,10 @@ while n > 0:
         page = wikipedia.page(title)
         art_url = page.url
         art_id = title
-        summary = ".".join(page.summary.split(".")[:8])
+        summary = page.summary
         full_text = art_url + "@@===#####===@@" + summary
 
-        if len(full_text) >= 300:
+        if len(full_text) >= 900:
             with open(f"output/{art_id}.txt", "w") as f:
                 f.write(full_text)
             print(f"Saved [{art_id}] ({len(full_text)} characters)")
